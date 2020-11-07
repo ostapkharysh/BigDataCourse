046a8bb4a01d1c03f193fe062f7a99cf40755d42
ostapkharysh <kharyshostap@gmail.com>
2020-11-07 19:53:57 +0200

    All 6/7 tasks made the right way using Spark RDDs. The 7th is wrong: should produce histogram, and class example code doesn't work too(

--- src/main/scala/sample/FirstExercise.scala
+++ src/main/scala/sample/FirstExercise.scala
@@ -1,6 +1,7 @@
 package sample
 
 import org.apache.avro.io.Encoder
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 import org.apache.spark.{SparkConf, SparkContext}
 
@@ -11,10 +12,32 @@ class FirstExercise {
   val sparkSession: SparkSession = SparkSession.builder.master("local")
     .appName("Manipulations with a file").getOrCreate()
 
-  val myRDD: DataFrame = sparkSession.read.text("pagecounts-20100806-030000")
+  val myRDD: RDD[String] = sc.textFile("pagecounts-20100806-030000")
+
+  val enPages: RDD[String] = myRDD.filter(line => line.split(" ")(0).equals("en"))
+  val enPagesTuples = enPages.flatMap(line =>{
+    val l = line.split(" ")
+    if (l.size == 4){
+      val lang = l(0)
+      val tit = l(1)
+      val req = l(2).toLong
+      val contSize = l(3).toLong
+      List((lang, tit, req, contSize))
+    }
+    else null
+  })
+
+  val MostVisitedPage: (String, String, Long, Long) =
+    enPagesTuples.sortBy(_._4, ascending = false).first()
 
 
-  def totalLines: Long = myRDD.count()
+
+
+
+
+
+
+  /*def totalLines: Long = myRDD.count()
 
   import sparkSession.implicits._
   val df: Dataset[(String, String, String, String)] = myRDD.map(f =>{
@@ -28,7 +51,7 @@ class FirstExercise {
                         .withColumnRenamed("_3", "requests")
                         .withColumnRenamed("_4", "contSize")
 
-  def enPages: Dataset[Row] = df2.filter($"lang" === "en")
+  def enPages: Dataset[Row] = df2.filter($"lang" === "en")*/
 
 
 
--- src/main/scala/sample/Main.scala
+++ src/main/scala/sample/Main.scala
@@ -2,6 +2,7 @@ package sample
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
 import sample.{IndianCode, SimpleApp}
 
 
@@ -21,8 +22,58 @@ object Main {
 
 
     val pageCount = new FirstExercise
-
     // 1st task
+    println("1. Top ten lines: ")
+    pageCount.myRDD.take(10).foreach(println(_))
+
+    //2nd task
+    println("2. Total lines")
+    println(pageCount.myRDD.count())
+
+    //3 task
+    println("3. Only english Wikipedia")
+    pageCount.enPages.take(30).foreach(println(_))
+
+    //4 task
+    println("4. Only english Wikipedia in Tuples")
+    pageCount.enPagesTuples.take(30).foreach(println(_))
+
+
+    //5 task
+    println("4. Only english Wikipedia in Tuples sorted by content size")
+    pageCount.enPagesTuples.sortBy(_._4, false)
+      .take(5).foreach(println(_))
+
+    //6 task
+    println("6. Most visited page")
+    println(pageCount.MostVisitedPage._2 +" "+ pageCount.MostVisitedPage._3)
+
+
+    //7 task
+    // Not mine: doesn't work even being from the example
+    def histogram(pageRdd : RDD[(String, String, Long, Long)], nBins : Int) : RDD[(Long, Int)] = {
+
+      // Fisrt, calculate the bounds (min and max)
+      val bounds = pageRdd.map(x => (x._3, x._3))
+        .reduce((t1, t2) => (math.min(t1._1, t2._1), math.max(t1._2, t2._2)))
+
+      val histRange = bounds._2-bounds._1
+      val binWidth = histRange/nBins
+
+      pageRdd
+        .map(t => {
+          val numReq = t._3
+          ((numReq - bounds._1)/binWidth)*binWidth+bounds._1
+        })
+        .groupBy(x=>x)
+        .map(t => (t._1, t._2.size))
+        .sortBy(_._1)
+    }
+
+    histogram(pageCount.enPagesTuples, 20).take(10).foreach(println)
+
+
+/*    // 1st task
     println("1. Top ten lines: ")
     pageCount.df2.printSchema()
     pageCount.df2.show(10)
@@ -34,7 +85,7 @@ object Main {
     //3 task
     println("3. Only english Wikipedia")
     pageCount.enPages.printSchema()
-    pageCount.enPages.show(30)
+    pageCount.enPages.show(30)*/
 
 
 
